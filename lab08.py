a0c5669356d717a0dd934aa5ff036b20177b8b70
ngthutrang194 <107092812+ngthutrang194@users.noreply.github.com>
2022-06-08 11:16:31 +0700

    Create lab08.py

---
+++ lab08.py
@@ -0,0 +1,8 @@
+# Mục tiêu trò chơi đoán số:
+
+# Cho phép người chơi dự đoán một số từ 1 đến 100.
+# Kiểm tra phỏng đoán của người dùng so với câu trả lời thực tế. In "Quá cao." hoặc "Quá thấp." tùy thuộc vào câu trả lời của người dùng.
+# Nếu họ có câu trả lời đúng, hãy hiển thị câu trả lời thực tế cho người chơi.
+# Theo dõi số lượt còn lại.
+# Nếu họ hết lượt, hãy cung cấp phản hồi cho người chơi.
+# Bao gồm hai mức độ khó khác nhau (ví dụ: 10 lần đoán ở chế độ dễ, chỉ 5 lần đoán ở chế độ khó).
